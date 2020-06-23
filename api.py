1245b151e82323cc8609bbd6af1a129322439a78
NiJia Lin <louis70109@gmail.com>
2020-06-23 17:13:30 +0800

    init

--- api.py
+++ api.py
@@ -1,14 +1,12 @@
 from flask import Flask
-from flask_cors import CORS
 from flask_restful import Api
 
-from controller.line_controller import LineIconSwitchController
+from controller.line_controller import LineGroupController
 
 app = Flask(__name__)
-CORS(app)
 
 api = Api(app)
-api.add_resource(LineIconSwitchController, '/webhooks/line')
+api.add_resource(LineGroupController, '/webhooks/line')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- controller/line_controller.py
+++ controller/line_controller.py
@@ -1,22 +1,17 @@
 import os
 from flask import request
-from flask_restful import Resource
 import json
+
+from flask_restful import Resource
 from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.models import (
-    TextSendMessage,
-    Sender)
-
-LINE_FRIEND = dict(
-    BROWN="https://stickershop.line-scdn.net/stickershop/v1/sticker/52002734/iPhone/sticker_key@2x.png",
-    CONY="https://stickershop.line-scdn.net/stickershop/v1/sticker/52002735/iPhone/sticker_key@2x.png",
-    SALLY="https://stickershop.line-scdn.net/stickershop/v1/sticker/52002736/iPhone/sticker_key@2x.png"
+    TextSendMessage, ImageSendMessage
 )
 
 
-class LineIconSwitchController(Resource):
+class LineGroupController(Resource):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -26,20 +21,33 @@ class LineIconSwitchController(Resource):
 
         line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
         handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
-        token = payload['events'][0]['replyToken']
-        msg = payload['events'][0]['message']['text'].upper()
+        event = payload['events'][0]
+        token = event['replyToken']
+        message = event['message']['text']
+        line_type = event['source'].get('type')
+        group = event['source'].get('groupId')
+        room = event['source'].get('roomId')
 
-        if msg in LINE_FRIEND:
-            name = msg
-            icon = LINE_FRIEND[msg]
-            text = TextSendMessage(
-                text=msg,
-                sender=Sender(
-                    name=name,
-                    icon_url=icon))
+        if line_type == 'group' and message == '群組資訊':
+            count = line_bot_api.get_group_members_count(group_id=group)
+            summary = line_bot_api.get_group_summary(group_id=group)
+            text = f'群組名稱➡️ {summary.group_name}\n當前群組人數為➡️ {count}'
+            line_bot_api.reply_message(
+                token, messages=[
+                    TextSendMessage(text=text),
+                    ImageSendMessage(
+                        original_content_url=summary.picture_url,
+                        preview_image_url=summary.picture_url,
+                    )]
+            )
+
+        elif line_type == 'room' and message == '對話群資訊':
+            count = line_bot_api.get_room_members_count(room_id=room)
+            text = f'對話群組人數為: {count}'
+            line_bot_api.reply_message(token, TextSendMessage(text=text))
+
         else:
-            text = TextSendMessage(text=msg)
-        line_bot_api.reply_message(token, text)
+            line_bot_api.reply_message(token, TextSendMessage(text=message))
 
         response = {
             "statusCode": 200,
