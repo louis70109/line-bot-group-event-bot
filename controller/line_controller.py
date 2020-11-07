7e9a019b0cf0029d0ec902b204ccfa8ae5cac2ce
NiJia <louis70109@gmail.com>
2020-11-08 01:46:08 +0800

    feat: unsend & join event

--- controller/line_controller.py
+++ controller/line_controller.py
@@ -22,24 +22,35 @@ class LineGroupController(Resource):
         line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
         handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
         event = payload['events'][0]
-        token = event['replyToken']
-        message = event['message']['text']
+
         line_type = event['source'].get('type')
         group = event['source'].get('groupId')
         room = event['source'].get('roomId')
         user = event['source'].get('userId')
+        if event.get('type') == 'unsend':
+            profile = line_bot_api.get_profile(user_id=user)
+            msg = f'{profile.display_name} 剛剛偷收回訊息！(抓)'
+            line_bot_api.push_message(to=group or room, messages=[TextSendMessage(text=msg)])
+            return 'OK'
 
+        token = event.get('replyToken')
+
+        message = event.get('message').get('text') if event.get('message') else ''
         if message == '你走吧':
+            msg = '走了88'
             if line_type == 'group':
-                line_bot_api.reply_message(token, TextSendMessage(text='走了88'))
+                line_bot_api.reply_message(token, TextSendMessage(text=msg))
                 line_bot_api.leave_group(group_id=group)
             elif line_type == 'room':
-                line_bot_api.reply_message(token, TextSendMessage(text='走了88'))
+                line_bot_api.reply_message(token, TextSendMessage(text=msg))
                 line_bot_api.leave_room(room_id=room)
             else:
-                line_bot_api.reply_message(token, TextSendMessage(text='為什麼不是你走？'))
+                msg = '為什麼不是你走？'
+                line_bot_api.reply_message(token, TextSendMessage(text=msg))
 
         elif line_type == 'group':
+            if event.get('type') == 'join':
+                message = '偶來ㄌ～'
             if message == '群組資訊':
                 count = line_bot_api.get_group_members_count(group_id=group)
                 summary = line_bot_api.get_group_summary(group_id=group)
