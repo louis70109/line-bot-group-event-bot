bcaa7e69d4a720a0f018e2a632f7c9b965897daa
NiJia <louis70109@gmail.com>
2020-11-08 16:03:17 +0800

    update: change to decorator

--- controller/line_controller.py
+++ controller/line_controller.py
@@ -2,13 +2,17 @@ import os
 from flask import request
 import json
 
-from flask_restful import Resource
+from flask_restful import Resource, abort
 from linebot import (
     LineBotApi, WebhookHandler
 )
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import (
-    TextSendMessage, ImageSendMessage
+    TextSendMessage, ImageSendMessage, VideoSendMessage, TextMessage, MessageEvent, JoinEvent, LeaveEvent
 )
+from linebot.models.events import UnsendEvent, VideoPlayCompleteEvent
+
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 
 
 class LineGroupController(Resource):
@@ -17,40 +21,76 @@ class LineGroupController(Resource):
         super().__init__(*args, **kwargs)
 
     def post(self):
-        payload = request.get_json(force=True)
+        body = request.get_data(as_text=True)
+        signature = request.headers['X-Line-Signature']
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            print("Invalid signature. Please check your channel access token/channel secret.")
+            abort(400)
 
+        return 'OK'
+
+    @handler.add(VideoPlayCompleteEvent)
+    def handle_follow(event):
         line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
-        handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
-        event = payload['events'][0]
 
-        line_type = event['source'].get('type')
-        group = event['source'].get('groupId')
-        room = event['source'].get('roomId')
-        user = event['source'].get('userId')
-        if event.get('type') == 'unsend':
-            profile = line_bot_api.get_profile(user_id=user)
-            msg = f'{profile.display_name} 剛剛偷收回訊息！(抓)'
-            line_bot_api.push_message(to=group or room, messages=[TextSendMessage(text=msg)])
-            return 'OK'
+        line_bot_api.reply_message(
+            event.reply_token,
+            messages=[TextSendMessage(text='喔齁齁齁看完了喔波波波')]
+        )
 
-        token = event.get('replyToken')
+    @handler.add(UnsendEvent)
+    def unsend_event(event):
+        line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+        line_type = event.source.type
+        group, room, user = None, None, None
+        if line_type == 'group':
+            group = event.source.group_id
+        elif line_type == 'room':
+            room = event.source.room_id
+        user = event.source.user_id
 
-        message = event.get('message').get('text') if event.get('message') else ''
+        profile = line_bot_api.get_profile(user_id=user)
+        msg = f'{profile.display_name} 剛剛偷收回訊息！(抓)'
+        line_bot_api.push_message(to=group or room, messages=[TextSendMessage(text=msg)])
+        return 'OK'
+
+    @handler.add(JoinEvent)
+    def join_event(event):
+        line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+        token = event.reply_token
+
+        line_bot_api.reply_message(token, TextSendMessage(text='偶來囉～～！'))
+        return 'OK'
+
+    @handler.add(MessageEvent or LeaveEvent, message=TextMessage)
+    def message_event(event):
+        line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+
+        line_type = event.source.type
+        group, room, user = None, None, None
+        if line_type == 'group':
+            group = event.source.group_id
+        elif line_type == 'room':
+            room = event.source.room_id
+        user = event.source.user_id
+
+        token = event.reply_token
+        message = event.message.text
+
         if message == '你走吧':
             msg = '走了88'
-            if line_type == 'group':
+            if group:
                 line_bot_api.reply_message(token, TextSendMessage(text=msg))
                 line_bot_api.leave_group(group_id=group)
-            elif line_type == 'room':
+            elif room:
                 line_bot_api.reply_message(token, TextSendMessage(text=msg))
                 line_bot_api.leave_room(room_id=room)
             else:
                 msg = '為什麼不是你走？'
                 line_bot_api.reply_message(token, TextSendMessage(text=msg))
-
-        elif line_type == 'group':
-            if event.get('type') == 'join':
-                message = '偶來ㄌ～'
+        elif group:
             if message == '群組資訊':
                 count = line_bot_api.get_group_members_count(group_id=group)
                 summary = line_bot_api.get_group_summary(group_id=group)
@@ -74,10 +114,16 @@ class LineGroupController(Resource):
                             preview_image_url=profile.picture_url,
                         )]
                 )
+            elif message == 'v1':
+                result = line_bot_api.set_webhook_endpoint(
+                    webhook_endpoint=f"{os.getenv('MY_DOMAIN')}/v1/webhooks/line"
+                )
+                if result == {}:
+                    message = '降版！'
+            else:
+                message = '請邀請我進群組喔\n指令為: \n1. 我是誰\n2.群組資訊\n3. 你走吧\n3. 輸入 v1 降版'
 
-            line_bot_api.reply_message(token, TextSendMessage(text=message))
-
-        elif line_type == 'room':
+        elif room:
             if message == '聊天室資訊':
                 count = line_bot_api.get_room_members_count(room_id=room)
                 text = f'聊天室人數為: {count}'
@@ -93,11 +139,26 @@ class LineGroupController(Resource):
                             preview_image_url=profile.picture_url,
                         )]
                 )
-
-            line_bot_api.reply_message(token, TextSendMessage(text=message))
-
+            elif message == 'v1':
+                result = line_bot_api.set_webhook_endpoint(
+                    webhook_endpoint=f"{os.getenv('MY_DOMAIN')}/v1/webhooks/line"
+                )
+                if result == {}:
+                    message = '降版！'
+            else:
+                message = '請邀請我進群組喔\n指令為: \n1. 我是誰\n2.群組資訊\n3. 你走吧\n3. 輸入 v1 降版'
         else:
-            line_bot_api.reply_message(token, TextSendMessage(text=message))
+            if message == 'video':
+                line_bot_api.reply_message(
+                    token,
+                    messages=VideoSendMessage(
+                        original_content_url='https://i.imgur.com/BhBshUO.mp4',
+                        preview_image_url='https://i.imgur.com/MW0Mpb6.jpg',
+                        tracking_id='duck')
+                )
+            else:
+                message = '請邀請我進群組喔\n指令為: \n1. 我是誰\n2.群組資訊\n3. 你走吧\n3. 輸入 v1 降版'
+        line_bot_api.reply_message(token, TextSendMessage(text=message))
 
         response = {
             "statusCode": 200,
